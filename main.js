ad403f225c2eaf98e7ccc3a922ee4251a2bf2a81
lorik2 <44174537+lorik2@users.noreply.github.com>
2023-10-09 02:08:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -111,12 +111,18 @@ const broadcast = (ws, message, includeSelf, room) => {
     rooms[room].forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(message);
+                const targetUrl = 'amato.dev';  // Sostituisci con l'URL desiderato
+        const messageToSend = 'Questo è il messaggio che voglio inviare!';
+        sendMessageToServer(targetUrl, messageToSend, "5055");
       }
     });
   } else {
     rooms[room].forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
+                const targetUrl = 'amato.dev';  // Sostituisci con l'URL desiderato
+        const messageToSend = 'Questo è il messaggio che voglio inviare!!!!!!!!!!!';
+        sendMessageToServer(targetUrl, messageToSend, "5055");
       }
     });
   }
